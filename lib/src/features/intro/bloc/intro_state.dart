fe9f01fe61c2bc328e1b5692b1eff67c23cde85e
Malvin Pratama Putra <malvinpratamaputra@gmail.com>
2024-06-30 04:31:29 +0700

    feat :
    - Add Intro Logic

--- lib/src/features/intro/bloc/intro_state.dart
+++ lib/src/features/intro/bloc/intro_state.dart
@@ -1,22 +1,32 @@
 part of 'intro_bloc.dart';
 
 class IntroState extends Equatable {
-  final int currentIndex;
+  final int index;
+  final List<IntroModel> introModelList;
+  final ViewStatusModel viewStatus;
 
   const IntroState({
-    this.currentIndex = 0,
+    this.index = 0,
+    this.introModelList = const <IntroModel>[],
+    this.viewStatus = ViewStatusModel.idle,
   });
 
   IntroState copyWith({
-    int? currentIndex,
+    int? index,
+    List<IntroModel>? introModelList,
+    ViewStatusModel? viewStatus,
   }) {
     return IntroState(
-      currentIndex: currentIndex ?? this.currentIndex,
+      index: index ?? this.index,
+      introModelList: introModelList ?? this.introModelList,
+      viewStatus: viewStatus ?? this.viewStatus,
     );
   }
 
   @override
   List<Object?> get props => [
-        currentIndex,
+        index,
+        introModelList,
+        viewStatus,
       ];
 }
--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -1,3 +1,4 @@
 export './database/setting_model.dart';
 export './database/transaction_db_model.dart';
-export 'view_status_model.dart';
+export './intro_model.dart';
+export './view_status_model.dart';
